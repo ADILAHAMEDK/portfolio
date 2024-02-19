99be9af63141ea15f236f3866c8bfb83401819f1
ADILAHAMEDK <adilahamedk5842@gmail.com>
2024-02-19 14:12:59 +0530

    portfolio

---
+++ js/protfolio.js
@@ -0,0 +1,27 @@
+// toggle menu icon when click => function
+let menuIcon = document.querySelector('#manu-icon')
+let navLinks = document.querySelector('.nav-bar')
+
+menuIcon.addEventListener('click', function () {
+    navLinks.classList.toggle('is-active')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
